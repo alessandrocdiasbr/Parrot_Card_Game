2443a6538871ab45940f647efceb0a85b076ee38
Alessandro Dias <acdias83@gmail.com>
2024-10-08 09:17:46 -0300

    inicio do código js

---
+++ parrotCardGame/js/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container');
+const main = document.querySelector('main'); 
+
+const images = [
+    'bobrossparrot',
+    'explodyparrot',
+    'fiestaparrot',
+    'metalparrot',
+    'revertitparrot',
+    'tripletsparrot',
+    'unicornparrot',
+];
+
