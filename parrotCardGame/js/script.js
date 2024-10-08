b2b56cc84a86efdbc10f2c7bfaffd3f36174a6b3
Alessandro Dias <acdias83@gmail.com>
2024-10-08 09:20:04 -0300

    criado função que pergunta número de cartas para jogar

--- parrotCardGame/js/script.js
+++ parrotCardGame/js/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-const main = document.querySelector('main'); 
+const main = document.querySelector('main');
 
 const images = [
     'bobrossparrot',
@@ -16,3 +16,12 @@ let secondCard = null;
 let moves = 0;
 let matchedPairs = 0;
 let lockBoard = false;
+
+function numCard() {
+    let numCards = parseInt(prompt("Digite um número par de cartas (entre 4 e 14):"));
+    while (isNaN(numCards) || numCards < 4 || numCards > 14 ||
+        numCards % 2 !== 0) { numCards = parseInt(prompt("Número inválido. Digite um número par de cartas (entre 4 e 14):")); }
+    return numCards;
+}
+
+const numCards = numCard();
